1064bbb4b368bbf455a79c4a6541f24c6d5d8ace
Hyosang56 <jhsaa01@naver.com>
2024-09-04 21:41:40 +0900

    Spring

---
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -0,0 +1,8 @@
+package com.office.library.admin.member;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminMemberController {
+
+}
---
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberDao.java
@@ -0,0 +1,11 @@
+package com.office.library.admin.member;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+
+@Component
+public class AdminMemberDao {
+
+}
