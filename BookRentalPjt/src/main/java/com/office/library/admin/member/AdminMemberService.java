1d1b9f72fea947629d469f92f302370d982885b5
Hyosang56 <jhsaa01@naver.com>
2024-09-05 21:25:24 +0900

    Spring

--- BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberService.java
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberService.java
@@ -4,5 +4,39 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdminMemberService {
+	
+	final static public int ADMIN_ACCOUNT_ALREADY_EXIST = 0;
+	   final static public int ADMIN_ACCOUNT_CREATE_SUCCESS = 1;
+	   final static public int ADMIN_ACCOUNT_CREATE_FAIL = -1;
+	   
+	   AdminMemberDao adminMemberDao = new AdminMemberDao();
+	public AdminMemberService() {	
+	}
+	public int createAccountConfirm(AdminMemberVo adminMemberVo) 
+	   {
+	      System.out.println(
+	   "[AdminMemberService] createAccountConfirm()");
+	      //중복 아이디를 확인합니다. isMember가 true이면 이미 사용중인 아이디이고,
+	      //false이면 사용가능한 아이디 입니다.
+	      boolean isMember = 
+	   adminMemberDao.isAdminMember(adminMemberVo.getA_m_id());
+	      
+	      if (!isMember) 
+	      {
+	         int result = 
+	   adminMemberDao.insertAdminAccount(adminMemberVo);
+	         
+	         if (result > 0)
+	            return ADMIN_ACCOUNT_CREATE_SUCCESS;
+	         
+	         else
+	            return ADMIN_ACCOUNT_CREATE_FAIL;
+	         
+	      } 
+	      else {
+	         return ADMIN_ACCOUNT_ALREADY_EXIST;
+	         
+	      }
+	   }//end of createAccountConfirm()
 
 }
