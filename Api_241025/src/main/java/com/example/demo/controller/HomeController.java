f47799df2d73bc631a2909d3888d88b983016d85
Hyosang56 <jhsaa01@naver.com>
2024-11-01 21:41:17 +0900

    spring

--- Api_241025/src/main/java/com/example/demo/controller/HomeController.java
+++ Api_241025/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.kakaoApi.Documents;
 import com.example.demo.kakaoApi.kakaoApiService;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
 
 @Controller
 public class HomeController {
@@ -23,15 +32,19 @@ public class HomeController {
 	@ResponseBody
 	// @ResponseBody가 없으면 String이 페이지인데
 	// @ResponseBody 때문에 호출되는 메서드에 리턴값이 데이터로 반환
-	public Documents searchNode(@RequestParam("keyword") String keyword) {
+	public List<Documents> searchNode(@RequestParam("keyword") String keyword) {
 		System.out.println("/searchNodeByKeyword 요청");
-		System.out.println("검색어: "+ keyword);
-		//keyword를 KAKAO API에 전송
+		//KAKAO API 키워드로 장소 검색하기
 		String result = kakaoApiService.searchplaceByKeyword(keyword);
-		//KAKAO API 반환되는 결과값 json
-		System.out.println(result);
-		//객체 형태로 변환
-		return null;
+		//String >> JsonObject 변환
+		Gson gson = new Gson();
+		JsonObject docs = JsonParser.parseString(result).getAsJsonObject();
+		// JsonObject의 key("documents")에 대한 value 수집
+		JsonArray documentArray = docs.get("documents").getAsJsonArray();
+		// JsonObjects >> Documents 형태 변환
+		Type listType = new TypeToken< List<Documents> >() {}.getType();
+		List<Documents> documentList = gson.fromJson(documentArray, listType);
+		return documentList;
 	}
 	
 }
